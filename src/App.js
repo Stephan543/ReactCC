df5fab7f1a3e11917773d08d42769858990df6f9
Stephan Iskander <stephan.iskander@gmail.com>
2021-02-13 13:29:05 -0500

    To-do app updated button/header components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import Header from './components/Header'
+
 function App() {
+  const name = "Stephan";
+  const x = true;
+
   return (
-    <div className="App">
-      <h1>Hello From React</h1>
+    <div className="container">
+      <Header />
+        {/* <h1>Hello From React</h1> */}
+      {/* <h2>Hello {x ? 'Yes': 'No'}</h2> */}
     </div>
   );
 }
 
+
+
 export default App;
+
+// CSS in JS
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black',
+// }
