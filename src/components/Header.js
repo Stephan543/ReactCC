7e1526e7868ab05d71a7a8b590b93148647285a7
Stephan Iskander <stephan.iskander@gmail.com>
2021-02-18 18:27:57 -0500

    Added JSON db and server, full stack functionality, can get/post data to database

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = ({ title, onAdd, showAdd}) => {
     return (
         <header className="header">
             <h1>{title}</h1>
-            <Button color={showAdd ? 'Red': 'Green'} text={showAdd ? 'Close': 'Add'} onClick= {onAdd} />
+            <Button color={showAdd ? 'Red': 'Greenn'} text={showAdd ? 'Close': 'Add'} onClick= {onAdd} />
         </header>
     )
 }
